02a737687eea654565371a8fea196cb6f3325ba4
Legacy810 <yidafdafid@gmail.com>
2024-11-14 20:01:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ global.qris = false
 
 
 // GLOBAL SETTING
-global.owner = "2347041039367"
+global.owner = "2347012007902"
 global.namabot = "*𝐁𝐋𝐔𝐄𝐃𝐄𝐌𝐎𝐍-𝐕𝟑*"
-global.nomorbot = "2347041039367"
+global.nomorbot = "2347012007902"
 global.namacreator = "*𝕯𝖊𝖒𝖔𝖓 𝖐𝖎𝖓𝖌* 👾"
 global.linkyt = false
 global.autoJoin = false
